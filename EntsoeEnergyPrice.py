50c8a58a56d6fc551e0e578ecf762d6524ed1439
Diego Galdino <dsgaldino.rj@gmail.com>
2023-05-15 10:28:41 +0200

    Update EntsoeEnergyPrice.py
    Follow the .ipynb file

--- EntsoeEnergyPrice.py
+++ EntsoeEnergyPrice.py
@@ -12,9 +12,7 @@
 
 
 import pandas as pd
-import requests
-import csv
-from datetime import timedelta, datetime, date
+from datetime import timedelta, datetime
 
 
 # #### Define the url
@@ -27,11 +25,34 @@ start_url = 'https://transparency.entsoe.eu/transmission-domain/r2/dayAheadPrice
 end_url = '+00:00|CET|DAY&biddingZone.values=CTY|10YNL----------L!BZN|10YNL----------L&resolution.values=PT60M&dateTime.timezone=CET_CEST&dateTime.timezone_input=CET+(UTC+1)+/+CEST+(UTC+2)'
 
 
+# #### Function Update Dataset
+
+# In[ ]:
+
+
+def update_dataset(dates, url_format, df_comb):
+    for actual_date in dates:
+        # Create a URL with the new date
+        url = start_url + actual_date.replace("-", ".") + end_url
+
+        # Copy the information from the URL into a temporary DataFrame
+        df_temp = pd.read_html(url)[0]
+
+        # Add a Date column to the temporary DataFrame
+        df_temp['Date'] = pd.to_datetime(actual_date, format=url_format).strftime("%Y-%m-%d")
+
+        # Append the information from the temporary DataFrame to the combined DataFrame
+        df_comb = pd.concat([df_comb, df_temp], ignore_index=True)
+
+    return df_comb
+
+
 # #### Read the csv file
 
-# In[3]:
+# In[ ]:
 
 
+# Read the original dataset
 df = pd.read_csv('EntsoeEnergyPrice.csv', parse_dates=['Date'], dayfirst=True)
 
 
@@ -40,28 +61,27 @@ df = pd.read_csv('EntsoeEnergyPrice.csv', parse_dates=['Date'], dayfirst=True)
 # In[4]:
 
 
+# Read the original dataset
+df = pd.read_csv('EntsoeEnergyPrice.csv', parse_dates=['Date'], dayfirst=True)
+
 # The earliest date in the dataset
-min_date = pd.to_datetime(df['Date'].min(), format='%Y-%m-%d').date()
+min_date = df['Date'].min().date()
 
-# The latest date in dataset
-max_date = pd.to_datetime(df['Date'].max(), format='%Y/%m/%d').date()
+# The latest date in the dataset
+max_date = df['Date'].max().date()
 
-# Today date
+# Today's date
 today = datetime.today().date()
 
 print('Earliest date found in the dataset:', min_date)
 print('Latest date found in the dataset:', max_date)
 print('Today\'s date:', today)
 
-
-# In[5]:
-
-
 # Create list of the days that are missing
-datesL = [] # Lastest Dates
-datesE = [] # Earlist Dates
+datesL = []  # Latest Dates
+datesE = []  # Earliest Dates
 
-# Days of difference between today and lastest date
+# Days of difference between today and latest date
 dif_days = (today - max_date).days
 
 # Check if the dataset is updated
@@ -70,22 +90,19 @@ if dif_days != 0:
 else:
     print('The dataset is updated!')
 
-# Updated the dates of interesting
-# Lastest Dates
-if dif_days != 0:
-    for i in range(dif_days):
-        max_date += timedelta(days=1)
-        datesL.append(max_date.strftime("%d-%m-%Y"))
-        
-# Earlist Dates
-for i in range(30):
-    min_date = min_date - timedelta(days=1)
-    datesE.append(min_date.strftime("%d-%m-%Y"))
 
-    
-print(min_date)
-print(max_date)    
-    
+# In[5]:
+
+
+# Updated the dates of interest
+# Latest Dates
+if dif_days != 0:
+    datesL = [(max_date + timedelta(days=i+1)).strftime("%d-%m-%Y") for i in range(dif_days)]
+
+# Earliest Dates
+datesE = [(min_date - timedelta(days=1)).strftime("%d-%m-%Y")]
+
+print("The list of days with data to be collected.")
 print(datesL)
 print(datesE)
 
@@ -96,36 +113,15 @@ print(datesE)
 
 
 # Create a temporary DataFrame to store the daily price 
-df_comb = pd.DataFrame() # Temporary DataFrame combine the prices with the original
-df_tempE = pd.DataFrame() # Temporary DataFrame for prices (earliest dates)
-df_tempL = pd.DataFrame() # Temporary DataFrame for prices (latest dates)
+df_comb = pd.DataFrame()  # Temporary DataFrame to combine the prices with the original
 
 # Updating the dataset with latest dates
 if dif_days != 0:
-    for actual_date in datesL:
-        # Create a URL with the new date
-        url = start_url + actual_date.replace("-", ".") + end_url
+    df_comb = update_dataset(datesL, "%d-%m-%Y", df_comb)
 
-        # Copy the information from the URL into a temporary DataFrame
-        df_tempL = pd.read_html(url)[0]
-
-        # Add a Date column to the temporary DataFrame
-        df_tempL['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-
-        # Append the information from the temporary DataFrame to the combined DataFrame
-        
-        df_comb = pd.concat([df_comb, df_tempL], ignore_index=True)
-
-        #     url = start_url + actual_date.replace("-", ".") + end_url
-
-# Copy the information from the URL into a temporary DataFrame
-df_tempE = pd.read_html(url)[0]
-
-# Add a Date column to the temporary DataFrame
-df_tempE['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-
-# Append the information from the temporary DataFrame to the combined DataFrame
-df_comb = pd.concat([df_comb, df_tempE], ignore_index=True)
+# Updating the dataset with earliest dates
+if min_date >= pd.to_datetime("2012-01-01").date():
+    df_comb = update_dataset(datesE, "%d-%m-%Y", df_comb)
 
 
 # #### Cleaning the data
@@ -133,51 +129,52 @@ df_comb = pd.concat([df_comb, df_tempE], ignore_index=True)
 # In[7]:
 
 
-#Drop the first level of columns
+# Drop the first level of columns
 df_comb.columns = df_comb.columns.droplevel()
 
-#Rename the Columns
-df_comb = df_comb.rename(columns={
+# Rename the columns
+column_mapping = {
     "index": "Index",
     "MTU": "start-end [time]",
     "Day-ahead Price": "Import Grid (EUR/kWh)",
     "": "Date"
-})
+}
 
-#Split the hours in start e end
+df_comb.rename(columns=column_mapping, inplace=True)
+
+# Split the hours in start and end
 df_comb[['Hour', 'End']] = df_comb['start-end [time]'].str.split('-', expand=True)
 
-#Remove blank spaces
+# Remove blank spaces
 df_comb['Hour'] = df_comb['Hour'].str.strip()
 df_comb['End'] = df_comb['End'].str.strip()
 
-#Remove the original column
+# Remove the original column
 df_comb.drop('start-end [time]', axis=1, inplace=True)
 df_comb.drop('End', axis=1, inplace=True)
 
-#Creating a new column Price and converting the MWh to KWh
+# Creating a new column "Price" and converting MWh to kWh
 df_comb['Import Grid (EUR/kWh)'] = df_comb['[EUR / MWh]'] / 1000
-df_comb = df_comb.drop('[EUR / MWh]', axis=1)
-
-
-# In[8]:
-
+df_comb.drop('[EUR / MWh]', axis=1, inplace=True)
 
 df_comb['Export Grid (EUR/kWh)'] = df_comb['Import Grid (EUR/kWh)']
 
-# Substituir valores maiores do que zero por 'ZERO' na coluna Export Grid (EUR/kWh)
+# Replace values greater than zero with 'ZERO' in the "Export Grid (EUR/kWh)" column
 df_comb.loc[df_comb['Import Grid (EUR/kWh)'] < 0, 'Import Grid (EUR/kWh)'] = 0
 
-# Substituir valores menores ou iguais a zero por zero na coluna Import Grid (EUR/kWh)
+# Replace values less than or equal to zero with zero in the "Import Grid (EUR/kWh)" column
 df_comb.loc[df_comb['Export Grid (EUR/kWh)'] > 0, 'Export Grid (EUR/kWh)'] = 0
-df_comb.loc[df_comb['Export Grid (EUR/kWh)'] < 0, 'Export Grid (EUR/kWh)'] = df_comb['Export Grid (EUR/kWh)']*(-1)
+df_comb.loc[df_comb['Export Grid (EUR/kWh)'] < 0, 'Export Grid (EUR/kWh)'] = df_comb['Export Grid (EUR/kWh)'] * (-1)
 
 
 # #### Salving the date in a CSV file
 
-# In[9]:
+# In[10]:
 
 
-# Save in a csv file
+# Append the information from the temporary DataFrame to the combined DataFrame
+df = pd.concat([df, df_comb], ignore_index=True)
+
+# Save to a CSV file
 df.to_csv('EntsoeEnergyPrice.csv', index=False)
 
