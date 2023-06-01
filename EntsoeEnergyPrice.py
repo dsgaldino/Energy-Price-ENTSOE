a96b37d8064ab6cb3e8c75219fb9f7db533feee0
Diego Galdino <dsgaldino.rj@gmail.com>
2023-06-01 10:33:12 +0200

    Update EntsoeEnergyPrice.py
    Follow the .ipynb

--- EntsoeEnergyPrice.py
+++ EntsoeEnergyPrice.py
@@ -3,8 +3,21 @@
 
 # # Energy Price
 # 
-# This application retrieves the price information provided by the Entsoe transparency portal, 
-# starting from the requested date up to today's date. The values are transformed from MWh to KWh.
+# The code is for an application that retrieves price information from the Entsoe transparency portal. It fetches price data starting from a requested date up to the current date. The values are converted from Megawatt-hour (MWh) to Kilowatt-hour (kWh).
+# 
+# The code imports the necessary libraries, defines the URL for accessing the price data, and defines two functions: **update_dataset** for updating the dataset and **convert_date** for converting values to date format.
+# 
+# Next, the code reads a CSV file named 'EntsoeEnergyPrice.csv' into a DataFrame called **df** and converts the **'Date'** column to date format.
+# 
+# Then, it determines the oldest and most recent dates present in the dataset, as well as today's date. The code checks if the dataset needs to be updated based on the difference between the most recent date and today.
+# 
+# After that, it creates lists of dates: **datesL** for the most recent dates and **datesE** for the oldest dates. The code retrieves the price information by calling the update_dataset function with the appropriate dates and updates the **df_comb** DataFrame.
+# 
+# Following that, the code performs data cleaning steps such as renaming columns, splitting columns, and converting price units.
+# 
+# Finally, the code concatenates the original DataFrame (**df**) with the updated DataFrame (**df_comb**), corrects the date formatting, and saves the resulting DataFrame to a CSV file named **'EntsoeEnergyPrice.csv'**.
+# 
+# *It's important to note that the code assumes the existence of the 'EntsoeEnergyPrice.csv' CSV file and may require modifications or additional dependencies to run correctly.*
 
 # #### Importing Libraries
 
@@ -12,6 +25,7 @@
 
 
 import pandas as pd
+import numpy as np
 from datetime import timedelta, datetime
 
 
@@ -25,7 +39,7 @@ start_url = 'https://transparency.entsoe.eu/transmission-domain/r2/dayAheadPrice
 end_url = '+00:00|CET|DAY&biddingZone.values=CTY|10YNL----------L!BZN|10YNL----------L&resolution.values=PT60M&dateTime.timezone=CET_CEST&dateTime.timezone_input=CET+(UTC+1)+/+CEST+(UTC+2)'
 
 
-# #### Function Update Dataset
+# #### Functions
 
 # In[3]:
 
@@ -47,20 +61,30 @@ def update_dataset(dates, url_format, df_comb):
     return df_comb
 
 
+# In[4]:
+
+
+def convert_date(value):
+    try:
+        return pd.to_datetime(value).date()
+    except ValueError:
+        return np.nan   
+
+
 # #### Read the csv file
 
 # In[5]:
 
 
 # Read the original dataset
-# df = pd.read_csv('EntsoeEnergyPrice.csv', parse_dates=['Date'], dayfirst=True)
-df = pd.read_csv('EntsoeEnergyPrice.csv')
+df = pd.read_csv('EntsoeEnergyPrice2.csv')
+df['Date'] = pd.to_datetime(df['Date'])
 df.head()
 
 
 # #### Define the Date
 
-# In[7]:
+# In[6]:
 
 
 df['Date'] = pd.to_datetime(df['Date'])
@@ -78,56 +102,67 @@ print('Earliest date found in the dataset:', min_date)
 print('Latest date found in the dataset:', max_date)
 print('Today\'s date:', today)
 
+
+# In[7]:
+
+
 # Create list of the days that are missing
 datesL = []  # Latest Dates
 datesE = []  # Earliest Dates
+
+# Create a temporary DataFrame to store the daily price 
+df_comb = pd.DataFrame() 
 
 # Days of difference between today and latest date
 dif_days = (today - max_date).days
 
 # Check if the dataset is updated
 if dif_days != 0:
-    print('The difference between the latest date and today is:', dif_days, 'days')
-else:
-    print('The dataset is updated!')
-
-
-# In[11]:
-
-
-# Updated the dates of interest
-# Latest Dates
-if dif_days != 0:
+    
+    # Create Latest Dates
     datesL = [(max_date + timedelta(days=i+1)).strftime("%d-%m-%Y") for i in range(dif_days)]
-
-# Earliest Dates
-datesE = [(min_date - timedelta(days=i)).strftime("%d-%m-%Y") for i in range(1, 91)] # Change the days range
-
-print("The list of days with data to be collected.")
-print(datesL)
-print(datesE)
-
-
-# #### Get the Price information based on the dates
-
-# In[12]:
-
-
-# Create a temporary DataFrame to store the daily price 
-df_comb = pd.DataFrame()  # Temporary DataFrame to combine the prices with the original
-
-# Updating the dataset with latest dates
-if dif_days != 0:
+    
+    # Updated the Latest Dates
     df_comb = update_dataset(datesL, "%d-%m-%Y", df_comb)
-
-# Updating the dataset with earliest dates
-if min_date >= pd.to_datetime("2012-01-01").date():
-    df_comb = update_dataset(datesE, "%d-%m-%Y", df_comb)
+    
+    # Print some information about the latest dates
+    print('The difference between the latest date and today is:', dif_days, 'days')
+    print("The list of latest days to be collected.")
+    print(datesL)
+    print('\n')
+    
+    # Create Earliest Dates
+    datesE = [(min_date - timedelta(days=i)).strftime("%d-%m-%Y") for i in range(1, 71)] # Change the days range
+    
+    # Updated the Earliest Dates
+    if min_date >= pd.to_datetime("2012-01-01").date():
+        df_comb = update_dataset(datesE, "%d-%m-%Y", df_comb)
+    
+    # Print some information about the Earliest dates    
+    print("The list of Earliest days to be collected.")
+    print(datesE)
+    print('\n')
+    
+else:
+    # Print some information about the latest dates
+    print('The dataset is updated with the most recent data!')
+    print('\n')
+    
+    # Create Earliest Dates
+    datesE = [(min_date - timedelta(days=i)).strftime("%d-%m-%Y") for i in range(1, 11)]
+    
+    # Updated the Earliest Dates
+    if min_date >= pd.to_datetime("2012-01-01").date():
+        df_comb = update_dataset(datesE, "%d-%m-%Y", df_comb)    
+    
+    # Print some information about the Earliest dates
+    print("The list of Earliest days to be collected.")
+    print(datesE)
 
 
 # #### Cleaning the data
 
-# In[ ]:
+# In[8]:
 
 
 # Drop the first level of columns
@@ -154,27 +189,42 @@ df_comb['End'] = df_comb['End'].str.strip()
 df_comb.drop('start-end [time]', axis=1, inplace=True)
 df_comb.drop('End', axis=1, inplace=True)
 
-# Creating a new column "Price" and converting MWh to kWh
+# Converting MWh to kWh and Drop the old colunm
 df_comb['Import Grid (EUR/kWh)'] = df_comb['[EUR / MWh]'] / 1000
 df_comb.drop('[EUR / MWh]', axis=1, inplace=True)
 
+# Creating a new column "Export Grid (EUR/kWh)" that is a copy of 'Import Grid (EUR/kWh)'
 df_comb['Export Grid (EUR/kWh)'] = df_comb['Import Grid (EUR/kWh)']
 
-# Replace values greater than zero with 'ZERO' in the "Export Grid (EUR/kWh)" column
-df_comb.loc[df_comb['Import Grid (EUR/kWh)'] < 0, 'Import Grid (EUR/kWh)'] = 0
+# Replace values greater than zero with NaN in the "Export Grid (EUR/kWh)" column
+df_comb.loc[df_comb['Import Grid (EUR/kWh)'] < 0, 'Import Grid (EUR/kWh)'] = np.nan
 
-# Replace values less than or equal to zero with zero in the "Import Grid (EUR/kWh)" column
-df_comb.loc[df_comb['Export Grid (EUR/kWh)'] > 0, 'Export Grid (EUR/kWh)'] = 0
+# Replace values less than or equal to zero with NaN in the "Import Grid (EUR/kWh)" column
+df_comb.loc[df_comb['Export Grid (EUR/kWh)'] > 0, 'Export Grid (EUR/kWh)'] = np.nan
 df_comb.loc[df_comb['Export Grid (EUR/kWh)'] < 0, 'Export Grid (EUR/kWh)'] = df_comb['Export Grid (EUR/kWh)'] * (-1)
 
 
-# #### Salving the date in a CSV file
+# #### Concatenate the Dataframes
 
-# In[ ]:
+# In[9]:
 
 
 # Append the information from the temporary DataFrame to the combined DataFrame
 df = pd.concat([df, df_comb], ignore_index=True)
+
+
+# #### Correction of the Date
+
+# In[10]:
+
+
+df['Date'] = df['Date'].apply(convert_date)
+
+
+# #### Salving the date in a CSV file
+
+# In[11]:
+
 
 # Save to a CSV file
 df.to_csv('EntsoeEnergyPrice.csv', index=False)
